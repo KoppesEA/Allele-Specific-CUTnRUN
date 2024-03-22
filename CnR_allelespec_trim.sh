6b1f205d3d28b6ffe9aa3e80b16bbcb449c94d22
Erik Koppes <41686327+KoppesEA@users.noreply.github.com>
2024-03-22 11:44:12 -0400

    Update CnR_allelespec_trim.sh

--- CnR_allelespec_trim.sh
+++ CnR_allelespec_trim.sh
@@ -4,7 +4,7 @@
 #SBATCH -t 1-00:00 # Runtime in D-HH:MM
 #SBATCH -J CnRtrim
 #SBATCH --output=CnRtrim-%A_%a.txt
-#SBATCH --cpus-per-task=1 # fastqc does not support multithread
+#SBATCH --cpus-per-task=4 # 4-cores
 #SBATCH --mem=16g # (see also --mem-per-cpu)
 #SBATCH --array=0-12 #13 samples
 
@@ -26,15 +26,16 @@ fqname=${names[${SLURM_ARRAY_TASK_ID}]}
 INPUT_FASTQ1=${fqname}_1.fastq.gz
 INPUT_FASTQ2=${fqname}_2.fastq.gz
 
+echo "performing quality and adapter trimming for paired-end reads:"
 echo $INPUT_FASTQ1
 echo $INPUT_FASTQ2
-echo $OUTPUT
 
 #Trim_galore: cutadapt
-mkdir $OUTPUT
 trim_galore \
 --paired \
 --fastqc \
+--quality 20 \
+--cores 4 \
 --gzip \
 --output $DIRtrim \
 $INPUT_FASTQ1 $INPUT_FASTQ2
