85926cb0bd7731ad72dd52d58ce39e93b0c1d5e7
Erik Koppes <41686327+KoppesEA@users.noreply.github.com>
2024-03-22 12:31:00 -0400

    trim edits

--- CnR_allelespec_bowtie2_align.sh
+++ CnR_allelespec_bowtie2_align.sh
@@ -15,31 +15,24 @@ DIRtrim=./trimfastq
 #make output directory
 mkdir $DIRtrim
 
-#Load trimgalore v0.6.5
-module load trimgalore/0.6.5
+#Load Bowtie2 v2.4.5
+module load bowtie2/2.4.5
 
 #Extract fq sample names from list text file
 names=($(cat CnR_MannLab_fqlist_02262024.txt))
 echo ${names[${SLURM_ARRAY_TASK_ID}]}
 
 fqname=${names[${SLURM_ARRAY_TASK_ID}]}
-INPUT_FASTQ1=$DIRfq/${fqname}_1.fastq.gz
-INPUT_FASTQ2=$DIRfq/${fqname}_2.fastq.gz
+INPUT_FASTQ1=${fqname}_1.fastq.gz
+INPUT_FASTQ2=${fqname}_2.fastq.gz
 
 echo "performing quality and adapter trimming for paired-end reads:"
 echo $INPUT_FASTQ1
 echo $INPUT_FASTQ2
 
-#Trim_galore: cutadapt
-trim_galore \
---paired \
---illumina \
---quality 20 \
---e 0.1 \
---phred33 \
---stringency 1 \
---cores 4 \
---gzip \
---fastqc \
---output $DIRtrim \
-$INPUT_FASTQ1 $INPUT_FASTQ2
+# Bowtie2 with CUT&RUN parameters
+bowtie2 \
+-p 4 \
+-x $BOWTIE_INDEX \
+-U $INPUT_FASTQ_CUTADAPT \
+-S ./Bowtie2cutUMIAdaptalignedm18_v99/${READ_BASE}.sam
